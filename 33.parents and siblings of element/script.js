e8f3bc881b86429fded244748a953717a18755a6
diptamoy mitra <mitradiptamoy70@gmail.com>
2022-12-30 21:51:08 +0530

    update

---
+++ 33.parents and siblings of element/script.js
@@ -0,0 +1,7 @@
+//head and body are siblings, both children of html
+
+console.log(document.body.firstChild) // #text
+a = document.body.firstChild
+console.log(a.parentNode) //body
+console.log(a.parentElement) //body
+console.log(a.firstChild.nextSibling)
