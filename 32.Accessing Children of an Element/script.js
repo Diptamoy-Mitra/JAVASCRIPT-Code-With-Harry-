108d75a5327f3fb84ee60abcf9d840bef7fa550b
diptamoy mitra <mitradiptamoy70@gmail.com>
2022-12-30 17:07:39 +0530

    update

---
+++ 32.Accessing Children of an Element/script.js
@@ -0,0 +1,13 @@
+console.log(document.body.firstChild) // chupa hua text ayegi
+
+console.log(document.body.lastChild)  //<script src="script.js"></script>
+
+console.log(document.body.childNodes)  //HTML collections   //NodeList(4) [text, div, text, script]
+let arr=Array.from(document.body.childNodes)
+console.log(arr) //(4) [text, div, text, script]
+
+//iterate arr
+// for (const iterator of arr) {
+//     console.log(iterator)
+// }
+console.log(document.hasChildNodes())  //true
