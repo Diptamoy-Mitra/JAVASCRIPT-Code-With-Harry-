d484a88d6ce2a1d5fe2d2005ad57a1ebc8424fd1
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-01-05 03:54:46 +0530

    ddd

---
+++ 41.innerHTML,outerHTML/script.js
@@ -0,0 +1,11 @@
+
+console.log(document.getElementsByTagName('span')[0]) //<span> hey I am span </span>  //show element in dom tree
+console.dir(document.getElementsByTagName('span')[0])  //shows element as object with its properties
+
+console.log(document.body.firstChild.nodeName)  //text
+console.log(first.innerHTML)
+first.innerHTML="hjnj"   //change innerHTML   //valid for elements nodes only 
+
+
+// document.body.firstChild.data
+// '\n  hello world!'
