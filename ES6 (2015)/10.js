e7c78e7a7e4f42ee9c4d93bb4fff15be0d71545d
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-03-27 16:10:29 +0530

    es6

---
+++ ES6 (2015)/10.js
@@ -0,0 +1,18 @@
+// PROMISES 
+
+
+
+//it is called callBack //it faces nesting problem and become complex
+// function login(cb){
+//   setTimeout(() => {
+//     console.log('Login...........')
+//     cb();
+//   }, 0);
+// }
+// login(()=>{
+//   console.log('Redirecting');    
+//   // Login...........
+//   // 10.js:12 Redirecting       
+
+// });
+
