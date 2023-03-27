2d9aa82adbfb35de6b28919e19de1cab4882e98c
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-03-27 16:10:33 +0530

    es6

--- ES6 (2015)/9.js
+++ ES6 (2015)/9.js
@@ -1,2 +1,28 @@
 // FOR OF LOOP
 
+//Array, string, iterable object, set, map
+
+
+const numbers=[2,3,5,7];
+
+for(const num of numbers){
+  console.log(num)
+}
+
+const languages='java'
+for(const char of languages){
+  console.log(char)
+}
+
+const person={
+  name: 'Rakesh',
+  city: 'Moscow'
+};
+// for (const it of person) {
+//   console.log(it)      //not iterable
+// }
+
+for (const it of Object.entries( person)) {
+    console.log(it)  //(2) ['city', 'Moscow']   
+  }
+  
